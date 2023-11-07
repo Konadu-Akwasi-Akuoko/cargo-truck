61d2708afa4a0d13f0733dca88b710e579628b53
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2023-11-07 08:50:58 +0000

    feat: created a MaxWidthWrapper to wrap all the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-     
+     <h1 className="font-bold text-5xl">Hello world</h1>
     </main>
   );
 }
