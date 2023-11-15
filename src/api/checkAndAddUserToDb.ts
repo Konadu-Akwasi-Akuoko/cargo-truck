f4a18f484c4f6fca2e566d4fff2ad11f2d11dbc0
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-11-15 11:23:32 +0000

    fix: fixed the build error

--- src/api/checkAndAddUserToDb.ts
+++ src/api/checkAndAddUserToDb.ts
@@ -1,11 +1,11 @@
-import { doc, setDoc } from "firebase/firestore"
-import { db } from "./firebase"
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 export const checkAndAddUserToDB = async () => {
-    // Add a new document in collection "cities"
-await setDoc(doc(db, "Users", "LA"), {
+  // Add a new document in collection "cities"
+  await setDoc(doc(db, "Users", "LA"), {
     name: "Los Angeles",
     state: "CA",
-    country: "USA"
-  })
-}
+    country: "USA",
+  });
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
-"use client";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 import { Button } from "@/components/ui/button";
 import {
   LoginLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/dashboard");
-
-  
+  redirect("/dashboard");
   return (
     <main className="">
       <MaxWidthWrapper className="flex flex-col gap-10 mt-10">
-        
         <div>
           <h2 className="text-2xl font-medium mb-6">
             Uh Oh. It seems you are not authenticated, sign in or sign up
